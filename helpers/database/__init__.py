3699c41456259f53f36e14c6acd3869cf98e79a2
JDavi-Dev <jdaviprofil@proton.me>
2025-08-07 18:39:40 -0300

    feat: Resgistar e configurar o flask-migrate

--- helpers/database/__init__.py
+++ helpers/database/__init__.py
@@ -3,9 +3,13 @@ from sqlalchemy.orm import DeclarativeBase
 
 from helpers.application import app
 
+from flask_migrate import Migrate
+
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:12345@localhost:5432/censoescolar"
 db.init_app(app)
+
+migrate = Migrate(app, db)
