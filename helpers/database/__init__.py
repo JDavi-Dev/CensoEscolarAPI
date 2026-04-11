8b4c564798a62bc25641a0a320d6c1a7cd169012
JDavi-Dev <jdaviprofil@proton.me>
2026-04-10 22:19:49 -0300

    refactor: Ajusta URI database para o serviço Docker

--- helpers/database/__init__.py
+++ helpers/database/__init__.py
@@ -9,7 +9,7 @@ class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:12345@localhost:5432/censoescolar"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:12345@db:5432/censoescolar"
 db.init_app(app)
 
 migrate = Migrate(app, db)
