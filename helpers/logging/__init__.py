0f1b45cfd454f387f2c3f145d8578fbf9fcaaa8a
JDavi-Dev <jdaviprofil@proton.me>
2026-04-10 22:21:51 -0300

    refactor: remove log em arquivo e usa stdout
    A gravação em arquivos .log causava conflitos no ambiente Docker. O logger foi simplificado para centralizar a saída no console, permitindo que o Docker trate a persistência e rotação dos logs nativamente.

--- helpers/logging/__init__.py
+++ helpers/logging/__init__.py
@@ -1,6 +1,5 @@
 import traceback
 import logging
-from logging.handlers import RotatingFileHandler
 
 # Create a logger with a unique name
 logger = logging.getLogger(__name__)
@@ -14,11 +13,6 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 streamHandler = logging.StreamHandler()  # Log messages to console
 streamHandler.setFormatter(formatter)
 logger.addHandler(streamHandler)
-
-# Define a RotatingFileHandler to log messages to a rotating set of files
-fileHandler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=3)
-fileHandler.setFormatter(formatter)
-logger.addHandler(fileHandler)
 
 # Função que aplica tabulação para as informações da exceção
 def log_exception(mensagem: str):
